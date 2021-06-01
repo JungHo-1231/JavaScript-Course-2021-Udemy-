a2181468e6da201548560ea3e18ed0e712cf81b0
Jung Ho <jungho1231@flexis.co.kr>
2021-06-01 17:53:41 +0900

    this keyword

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,38 +1,90 @@
 'use strict';
-let rollDiceNumber = Math.trunc(Math.random() * 6) + 1;
-let leftScore = document.querySelector('#score--0').textContent;
-let rightScore = document.querySelector('#score--1').textContent;
 
-console.log(rollDiceNumber);
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
 
-function diceImageSetting(imageFileName) {
-    document.querySelector('.dice').src = imageFileName;
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
 }
 
-if (rollDiceNumber === 1){
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+    if (playing) {
+        // 1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-} else {
-    // todo
-    leftScore += rollDiceNumber;
-}
+        // 2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
 
-switch (rollDiceNumber) {
-    case 1 :
-        diceImageSetting("dice-1.png");
-        break;
-    case 2 :
-        diceImageSetting("dice-2.png");
-        break;
-    case 3 :
-        diceImageSetting("dice-3.png");
-        break;
-    case 4 :
-        diceImageSetting("dice-4.png");
-        break;
-    case 5 :
-        diceImageSetting("dice-5.png");
-        break;
-    case 6 :
-        diceImageSetting("dice-6.png");
-        break;
-}
+        // 3. Check for rolled 1: if ture, switch to next player
+        if (dice !== 1) {
+            // Add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore
+        } else {
+            // Switch to next player
+            switchPlayer();
+        }
+    }
+});
+
+btnHold.addEventListener("click", function () {
+    if (playing) {
+        // 1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
+
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+
+        // 2. check if player's score is >= 100
+        if (scores[activePlayer] >= 20) {
+            // finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        } else {
+            // Finish the game
+            // Switch to the next player
+            switchPlayer();
+        }
+    }
+});
+
+btnNew.addEventListener("click", function () {
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    currentScore = 0;
+    scores[0] = 0;
+    scores[1] = 0;
+    activePlayer = 0;
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player-active");
+});
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const myName = "Jonas";
+
+if (myName === 'Jonas') {
+    const job = 'teacher';
+    console.log(`Jone is a ${job}`);
+    const age = 2037 - 1989;
+    console.log(age);
+    // console.log(x);
+}
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "jonas";
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+const numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
